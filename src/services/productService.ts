30717e61036b5dc56d222e4244fe495495813af9
harshilprajapati-1994 <harshilprajapati135@gmail.com>
2025-06-17 13:39:58 +0530

    ecommerce-demo

--- src/services/productService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import type { Product } from '../types/Product';
-
-export const fetchProducts = async (): Promise<Product[]> => {
-  const res = await axios.get<Product[]>('https://fakestoreapi.com/products');
-  console.log(res.data)
-  return res.data;
-};
