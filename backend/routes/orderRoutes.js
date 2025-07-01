5b94d3db14d2399dc8b0a96ebe480ddc2317fa88
harshilprajapati-1994 <harshilprajapati135@gmail.com>
2025-07-01 17:26:21 +0530

    Ecommerce Site from backend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,18 +1,15 @@
+"use strict";
 const express = require('express');
 const router = express.Router();
-
 let orders = [];
-
 router.post('/', (req, res) => {
     const order = req.body;
     orders.push(order);
     res.status(201).json({ message: 'Order placed successfully' });
-})
-
-router.get('/:email', (req, res) => {
-  const email = req.params.email;
-  const userOrders = orders.filter(order => order.email === email);
-  res.json(userOrders);
 });
-
+router.get('/:email', (req, res) => {
+    const email = req.params.email;
+    const userOrders = orders.filter(order => order.email === email);
+    res.json(userOrders);
+});
 module.exports = router;
